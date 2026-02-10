7da7e5caba76f527b8ba1fffa50f6f8603035903
Djan Carlo C. Palmero <palmero.djancarlo@marsu.edu.ph>
2026-02-10 16:54:05 +0800

    fixed db.js connection

--- server.js
+++ server.js
@@ -1,20 +1,19 @@
 //server.js
 require("dotenv").config();
 const express = require('express');
-const rooms = require("./src/models/dishModels");
+const connectDB = require('./src/config/db');
+const apiRoutes = require('./src/routes/apiRoutes');
 const app = express();
-
+connectDB();
 
 //Middleware
 app.use(express.json());
-
-
-const apiRoutes = require('./src/Routes/apiRoutes');
+app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 const BASE_URI = process.env.BASE_URI || '/api/v1';
 
-app.use(BASE_URI, apiRoutes);
+app.use(process.env.BASE_URI, apiRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
